33a88dc4e92829b56db27750bf46e2f6cb9a4cdb
joruughen <jorge.melgarejo@utec.edu.pe>
2025-06-07 00:09:43 -0500

    Solo irá el backend en este repositorio

--- frontend/math-tutor-ai-frontend/src/Components/UI/label.tsx
+++
@@ -1,8 +0,0 @@
-import { cn } from "../../Lib/Util";
-import React from "react"
-
-export const Label = ({ className, ...props }: React.LabelHTMLAttributes<HTMLLabelElement>) => {
-  return (
-    <label className={cn("text-sm font-medium text-gray-700", className)} {...props} />
-  )
-}
--- mvp/src/components/ui/Label.tsx
+++
@@ -1,8 +0,0 @@
-import { cn } from "../../utils";
-import React from "react"
-
-export const Label = ({ className, ...props }: React.LabelHTMLAttributes<HTMLLabelElement>) => {
-  return (
-    <label className={cn("text-sm font-medium text-gray-700", className)} {...props} />
-  )
-}
