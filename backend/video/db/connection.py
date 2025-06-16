12e8e48c058ba772e6ab6f063fc158df685174c3
zamir5895 <lmr293141@gmail.com>
2025-06-16 14:09:32 -0500

    adding

--- backend/video/db/connection.py
+++
@@ -1,7 +0,0 @@
-from pydantic_settings import BaseSettings
-
-class Settings(BaseSettings):
-    MONGODB_URL: str = "mongodb://localhost:27017"
-    MONGODB_DB_NAME: str = "tema_service"
-
-settings = Settings()
