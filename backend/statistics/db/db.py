0a0844972b0e058b52325db18434760eaadcc100
zamir5895 <lmr293141@gmail.com>
2025-06-20 17:39:16 -0500

    f

--- backend/statistics/db/db.py
+++ backend/statistics/db/db.py
@@ -4,13 +4,12 @@ import os
 
 load_dotenv() 
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/learning_platform")
+MONGO_URI = os.getenv("MONGO_URI")
+MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "learning_platform")
 
 class MongoDB:
     def __init__(self, uri=MONGO_URI):
         self.client = AsyncIOMotorClient(uri)
-        # Extrae el nombre de la base de datos del URI si está presente
-        db_name = uri.rsplit('/', 1)[-1] if '/' in uri else "learning_platform"
-        self.db = self.client[db_name]
+        self.db = self.client[MONGO_DB_NAME]
 
 mongo = MongoDB()
