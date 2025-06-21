2ab91f391f8677f9e42c1338c36c0c6b54fed973
zamir5895 <lmr293141@gmail.com>
2025-06-20 22:43:40 -0500

    ih

--- backend/Ejercicios/db/config.py
+++ backend/Ejercicios/db/config.py
@@ -9,18 +9,10 @@ from dotenv import load_dotenv
 load_dotenv()  
 
 class Settings(BaseSettings):
-<<<<<<< HEAD
     mongo_uri: Optional[str] = os.getenv("MONGO_URI")
     youtube_api_key: Optional[str] = os.getenv("YOUTUBE_API_KEY")
     github_token: Optional[str] = os.getenv("GITHUB_TOKEN")
     database_name: str = "learning_platform"
-    load_dotenv()
-=======
-    mongo_uri: Optional[str]      = os.getenv("MONGO_URI", "mongodb://localhost:27017")
-    youtube_api_key: Optional[str] = os.getenv("YOUTUBE_API_KEY", "YOUR_YOUTUBE_API_KEY")
-    database_name: str             = "learning_platform"
-
->>>>>>> 2d81259d9b60f28aa685c8ea5f29805422334da7
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
